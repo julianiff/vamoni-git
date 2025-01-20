acee1f0af9929c4f973d19f32a07de23b77b3092
julianiff <julian.iff@frontify.com>
2025-01-20 22:06:11 +0100

    chore: add changes

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"reflect"
+	"slices"
+	"strings"
 )
 
 // vamoni init -- creates a .vamoni folder that holds the local config
@@ -15,7 +19,9 @@ import (
 // if line are not same, print new line
 
 // vamoni change "creates a new change of the repository"
-// --> cp all files into .vamoni folder (we can later compress)
+
+// --> every change has a corresponding change directory in .vamoni/change
+// --> cp all files into .vamoni folder (we can later compressu
 // change -> a change is the delta of files before and files after
 // 0 files -> 3 files -- change is the 3 new files
 
@@ -25,6 +31,88 @@ func vamoniInit() {
 	}
 
 	fmt.Println("vamoni folder created")
+}
+
+func diff(workingDir []string, storedDir []string) []string {
+	var difference []string
+	for _, w1 := range workingDir {
+		if !slices.Contains(storedDir, w1) {
+			difference = append(difference, w1)
+		}
+	}
+
+	return difference
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func detect() []string {
+	filesStored, err := os.ReadDir(".vamoni/change")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var filesStoredNames []string
+	for _, s := range filesStored {
+		filesStoredNames = append(filesStoredNames, s.Name())
+
+	}
+
+	files, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var changedFiles []string
+	for _, s := range files {
+		if !strings.HasPrefix(s.Name(), ".") {
+			changedFiles = append(changedFiles, s.Name())
+		}
+	}
+
+	equal := reflect.DeepEqual(changedFiles, filesStored)
+	fmt.Println("These files changed", equal)
+	output := diff(changedFiles, filesStoredNames)
+	fmt.Println("these file are changed", output)
+
+	return output
+	// fmt.Println(output)
+	// for _, f := range output {
+	// 	dat, err := os.ReadFile(f)
+	// 	check(err)
+	// 	fmt.Println(string(dat))
+	// }
+}
+
+func copyfile(sourceFile string, destinationFile string) {
+
+	source, err := os.Open(sourceFile)
+	if err != nil {
+		panic(err)
+	}
+	defer source.Close()
+
+	destination, err := os.Create(destinationFile)
+	if err != nil {
+		panic(err)
+	}
+
+	defer destination.Close()
+	_, err = io.Copy(destination, source)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func change() {
+	fileWithchanges := detect()
+	for _, f := range fileWithchanges {
+		copyfile(f, ".vamoni/change/"+f)
+	}
+	fmt.Println("New files added to changeset", fileWithchanges)
 }
 
 func main() {
@@ -39,9 +127,9 @@ func main() {
 	case "init":
 		vamoniInit()
 	case "detect":
-		fmt.Println("detect")
+		detect()
 	case "change":
-		fmt.Println("change")
+		change()
 	default:
 		fmt.Println("No valid method names provided")
 		os.Exit(1)
