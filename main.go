49d47f1e34074be0e4ee5cfda52528ad7d9e5e2a
julianiff <julian.iff@frontify.com>
2025-01-21 21:43:36 +0100

    chore: make a simple status, stage, change flow

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"reflect"
 	"slices"
 	"strings"
 )
@@ -30,61 +29,16 @@ func vamoniInit() {
 		log.Fatal(err)
 	}
 
+	if err := os.MkdirAll(".vamoni/stage", os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("vamoni folder created")
 }
 
-func diff(workingDir []string, storedDir []string) []string {
-	var difference []string
-	for _, w1 := range workingDir {
-		if !slices.Contains(storedDir, w1) {
-			difference = append(difference, w1)
-		}
-	}
-
-	return difference
-}
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-func detect() []string {
-	filesStored, err := os.ReadDir(".vamoni/change")
-	if err != nil {
-		log.Fatal(err)
-	}
-	var filesStoredNames []string
-	for _, s := range filesStored {
-		filesStoredNames = append(filesStoredNames, s.Name())
-
-	}
-
-	files, err := os.ReadDir(".")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var changedFiles []string
-	for _, s := range files {
-		if !strings.HasPrefix(s.Name(), ".") {
-			changedFiles = append(changedFiles, s.Name())
-		}
-	}
-
-	equal := reflect.DeepEqual(changedFiles, filesStored)
-	fmt.Println("These files changed", equal)
-	output := diff(changedFiles, filesStoredNames)
-	fmt.Println("these file are changed", output)
-
-	return output
-	// fmt.Println(output)
-	// for _, f := range output {
-	// 	dat, err := os.ReadFile(f)
-	// 	check(err)
-	// 	fmt.Println(string(dat))
-	// }
+func status() {
+	detect()
+	allStagedFiles := stagedFiles()
+	fmt.Println("these files are staged", allStagedFiles)
 }
 
 func copyfile(sourceFile string, destinationFile string) {
@@ -107,12 +61,97 @@ func copyfile(sourceFile string, destinationFile string) {
 	}
 }
 
-func change() {
-	fileWithchanges := detect()
-	for _, f := range fileWithchanges {
-		copyfile(f, ".vamoni/change/"+f)
+func diff(workingDir []string, storedDir []string) []string {
+	var difference []string
+	for _, w1 := range workingDir {
+		if !slices.Contains(storedDir, w1) {
+			difference = append(difference, w1)
+		}
 	}
-	fmt.Println("New files added to changeset", fileWithchanges)
+
+	return difference
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func editedFiles(path string) []string {
+
+	filesStored, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var filesStoredNames []string
+	for _, s := range filesStored {
+		filesStoredNames = append(filesStoredNames, s.Name())
+	}
+
+	files, err := os.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var changedFiles []string
+	for _, s := range files {
+		if !strings.HasPrefix(s.Name(), ".") {
+			changedFiles = append(changedFiles, s.Name())
+		}
+	}
+
+	return diff(changedFiles, filesStoredNames)
+}
+
+func detect() {
+	output := editedFiles(".vamoni/change")
+	fmt.Println("these files are changed", output)
+}
+
+func stagedFiles() []string {
+	stageFiles, err := os.ReadDir(".vamoni/stage")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var allStagedFiles []string
+	for _, s := range stageFiles {
+		allStagedFiles = append(allStagedFiles, s.Name())
+	}
+	return allStagedFiles
+}
+
+func stage(fileToStage string) {
+	allEditedFiles := editedFiles(".vamoni/change")
+	allStagedFiles := stagedFiles()
+
+	// only files that are edited can be staged
+	if slices.Contains(allEditedFiles, fileToStage) && !slices.Contains(allStagedFiles, fileToStage) {
+		fmt.Println("file can be staged", fileToStage)
+		copyfile(fileToStage, ".vamoni/stage/"+fileToStage)
+	}
+
+	newlyStagedFiles := stagedFiles()
+	fmt.Println("Currently Staged Files", newlyStagedFiles)
+	fmt.Println("files that can be staged", allEditedFiles)
+}
+
+func change() {
+	stageFiles, err := os.ReadDir(".vamoni/stage")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var allStagedFiles []string
+	for _, s := range stageFiles {
+		allStagedFiles = append(allStagedFiles, s.Name())
+	}
+
+	for _, f := range allStagedFiles {
+		copyfile(f, ".vamoni/change/"+f)
+		os.Remove(".vamoni/stage/" + f)
+	}
+
+	fmt.Println("New files added to changeset", allStagedFiles)
 }
 
 func main() {
@@ -126,8 +165,13 @@ func main() {
 
 	case "init":
 		vamoniInit()
+	case "status":
+		status()
 	case "detect":
 		detect()
+	case "stage":
+		args2 := os.Args[2]
+		stage(args2)
 	case "change":
 		change()
 	default:
